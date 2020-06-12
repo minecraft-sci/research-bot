68c03bde1cbb9578483a6866a68a8f9a3a1929a6
vcokltfre <vcokltfre@gmail.com>
2020-06-12 08:45:48 +0100

    Added !info command

--- cogs/general.py
+++ cogs/general.py
@@ -1,13 +1,26 @@
 import discord
 from discord.ext import commands
+import time
 
 class General(commands.Cog):
     def __init__(self, bot: commands.Bot):
         self.bot = bot
+        self.initTime = round(time.time())
 
     @commands.Cog.listener()
     async def on_ready(self):
         await self.bot.change_presence(activity=discord.Game(name="on Minecraft Research"))
+
+    @commands.command(name="info")
+    @commands.has_any_role("Member", "Moderator", "Administrator")
+    async def info(self, ctx):
+        embed = discord.Embed(title="Research Bot Info", description="Useful info and stats for ResBot")
+        uptime = round(time.time() - self.initTime)
+        hours = uptime // 3600
+        minutes = (uptime - (hours * 3600)) // 60
+        seconds = uptime - (hours * 3600 + minutes * 60)
+        embed.add_field(name="Uptime", value=f"{hours} hours, {minutes} minutes, {seconds} seconds")
+        await ctx.channel.send(embed=embed)
 
 def setup(bot):
     bot.add_cog(General(bot))
