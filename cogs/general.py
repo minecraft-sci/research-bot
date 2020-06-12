7bcffc532e0909f8b3be683f8c7bb8f1e8837f8f
vcokltfre <vcokltfre@gmail.com>
2020-06-12 08:35:59 +0100

    Added general cog

---
+++ cogs/general.py
@@ -0,0 +1,13 @@
+import discord
+from discord.ext import commands
+
+class General(commands.Cog):
+    def __init__(self, bot: commands.Bot):
+        self.bot = bot
+
+    @commands.Cog.listener()
+    async def on_ready(self):
+        await self.bot.change_presence(activity=discord.Game(name="on Minecraft Research"))
+
+def setup(bot):
+    bot.add_cog(General(bot))
