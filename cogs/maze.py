d3a1a056f013c4e50c67501bc000fdda127633f4
vcokltfre <vcokltfre@gmail.com>
2020-06-14 20:49:56 +0100

    changed maze to a command group

--- cogs/maze.py
+++ cogs/maze.py
@@ -18,7 +18,11 @@ class Maze(commands.Cog):
 			if self.rng.randint(0,100) == 69:
 				await ctx.channel.send(f"Do you like Alex Dillinger {ctx.author.mention}")
 
-	@commands.command(name='togglemaze') # What is the point in commands.has_any_role if it checks by id anyway?
+	@commands.group(name="maze")
+	async def maze(self, ctx):
+		pass
+
+	@maze.command(name='toggle') # What is the point in commands.has_any_role if it checks by id anyway?
 	async def togglemaze(self, ctx):
 		if ctx.author.id in self.mid or ctx.author.id == 383931730211504128 or ctx.author.id == 297045071457681409: # haha my id go brrrrr
 			self.enabled = not self.enabled
