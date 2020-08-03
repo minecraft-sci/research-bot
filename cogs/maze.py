a6f10d24dd1b3e9f8d26f4ac34efb2bc63b156bf
7x-0 <68181871+7x-0@users.noreply.github.com>
2020-08-03 16:44:06 +0545

    Update maze.py
    Eww... cringe added.

--- cogs/maze.py
+++ cogs/maze.py
@@ -8,6 +8,7 @@ class Maze(commands.Cog):
         # we define the enablement toggle
 		self.enabled = True
 		self.mid = [373946864531144726]
+		self.nooooooooo = [738981683516145785]
 
 	@commands.Cog.listener()
 	async def on_message(self, ctx):
@@ -17,14 +18,22 @@ class Maze(commands.Cog):
 				await ctx.channel.send(f"Fuck you {ctx.author.mention}")
 			if self.rng.randint(0,100) == 69:
 				await ctx.channel.send(f"Do you like Alex Dillinger {ctx.author.mention}")
+				
+		if ctx.author.id in self.nooooooooo and self.enabled:
+			# Why are you taking this risk?
+			# -> Yes.
+			
+			if self.rng.randint(1,100) in [7,77]:
+				await ctx.channel.send(f"Ewww cringe. {ctx.author.mention}")
+		
 
 	@commands.group(name="maze")
 	async def maze(self, ctx):
 		pass
 
 	@maze.command(name='toggle') # What is the point in commands.has_any_role if it checks by id anyway?
-	async def togglemaze(self, ctx):
-		if ctx.author.id in self.mid or ctx.author.id == 383931730211504128 or ctx.author.id == 297045071457681409: # haha my id go brrrrr
+	async def togglemaize(self, ctx):
+		if ctx.author.id in self.mid or ctx.author.id in [383931730211504128,297045071457681409,738981683516145785]: # haha my id go brrrrr, haha my id also go brrrrr
 			self.enabled = not self.enabled
 			await ctx.send(f"Toggled thonk responses to {self.enabled}")
 
